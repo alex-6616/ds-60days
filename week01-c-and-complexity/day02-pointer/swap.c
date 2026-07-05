b7ca67053c3fd46be78a589bc70e03b3a44f9ced
alex-6616 <811827647@qq.com>
2026-07-05 22:32:59 +0800

    Update swap.c

--- week01-c-and-complexity/day02-pointer/swap.c
+++ week01-c-and-complexity/day02-pointer/swap.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+void swap(int *pa, int *pb) {
+    int temp = *pa;
+    *pa = *pb;
+    *pb = temp;
+}
+
+int main() {
+    int x, y;
+    scanf("%d %d", &x, &y);
+
+    swap(&x, &y);
+
+    printf("%d %d\n", x, y);
+
+    return 0;
+}
